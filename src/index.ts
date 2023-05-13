b38d77519b4f0da6db71de85cb11047569ddab09
Jacob Hands <jacob@gogit.io>
2023-05-13 18:35:42 -0500

    Lower s-maxage

--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,7 @@ const kvApp = new Hono<App & { Variables: { kv: KVNamespace } }>()
 			'Content-Type',
 			kvRes.metadata?.headers['Content-Type'] || 'application/octet-stream'
     )
-    c.header('Cache-Control', 'public, max-age=60, s-maxage=86400')
+    c.header('Cache-Control', 'public, max-age=60, s-maxage=60')
     const response = c.body(kvRes.value)
 
 		c.executionCtx.waitUntil(cache.put(c.req.url, response.clone()))
